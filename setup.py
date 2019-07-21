29599652b6585da5cc850375b0da248f36c1ff18
David Halls <dahalls@gmail.com>
2019-07-21 23:04:06 +0100

    Rev version, update docs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(name):
 
 setup(
     name='python-dxf',
-    version='7.5.0',
+    version='7.5.1',
     description="Package for accessing a Docker v2 registry",
     long_description=read('README.rst'),
     keywords='docker registry',
